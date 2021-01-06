46dfdbbd93051da4f1c3711d5d2296824531a929
sholabioku <bilushonline@gmail.com>
2021-01-06 17:32:00 +0100

    Use morgan and helmet middleware

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const genres = require('./routes/genres');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+}
 
 app.use('/api/genres', genres);
 
