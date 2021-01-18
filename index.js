6490167f664cd656f7f6f61ca95d8f77b9adcad2
sholabioku <bilushonline@gmail.com>
2021-01-18 14:18:57 +0100

    Add CRUD functionality for movie API and persist it to database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const app = express();
@@ -24,6 +25,7 @@ if (app.get('env') === 'development') {
 
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
