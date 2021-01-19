d4535a6a9e24f4e5cf704a94a62b4446d0ebceee
sholabioku <bilushonline@gmail.com>
2021-01-19 10:47:00 +0100

    Add user model and its functionality

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 1024,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(1024).required(),
+  });
+
+  return schema.validate(user);
+}
+
+exports.User = User;
+exports.userSchema = userSchema;
+exports.validate = validateUser;
