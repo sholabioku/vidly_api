669ff7f50ad946951a9f9612de989a21f3ef664b
sholabioku <bilushonline@gmail.com>
2021-01-20 17:35:09 +0100

    Set response headers

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const router = express.Router();
@@ -17,7 +19,12 @@ router.post('/', async (req, res) => {
   user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
-  res.send(_.pick(user, ['_id', 'name', 'email']));
+
+  const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+
+  res
+    .header('x-auth-token', token)
+    .send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
