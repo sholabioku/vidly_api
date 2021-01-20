eb9b4a9569abd270540a8b2cf8faca44b0e9b52e
sholabioku <bilushonline@gmail.com>
2021-01-20 17:07:02 +0100

    Add token

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const router = express.Router();
 
@@ -14,7 +15,9 @@ router.post('/', async (req, res) => {
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send('Invalid email or password');
-  res.send(true);
+
+  const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey');
+  res.send(token);
 });
 
 function validate(req) {
