f17ae72060a793dc0a4db80e10a2138f01255567
sholabioku <bilushonline@gmail.com>
2021-01-20 16:16:04 +0100

    Mount /api/auth route

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 mongoose
   .connect('mongodb://localhost/vidly', {
@@ -35,6 +36,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
