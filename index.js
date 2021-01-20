2791d4335fdbe237b1e45a09ce5cd80eabdfb22d
sholabioku <bilushonline@gmail.com>
2021-01-20 15:20:40 +0100

    Add POST /api/users route with its functionality

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 
 mongoose
   .connect('mongodb://localhost/vidly', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
+    useCreateIndex: true,
   })
   .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.error('Could not connect to MongoDB...'));
@@ -32,6 +34,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
