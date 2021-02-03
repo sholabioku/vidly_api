cce585c6b50461a4e2828a546c88713d45125ae8
sholabioku <bilushonline@gmail.com>
2021-02-03 10:47:13 +0100

    Refracture to validateObjectId and use it

--- routes/genres.js
+++ routes/genres.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
+const validateObjectId = require('../middleware/validateObjectId');
 
 const { Genre, validate } = require('../models/genre');
 
@@ -49,10 +50,7 @@ router.delete('/:id', [auth, admin], async (req, res) => {
   res.send(genre);
 });
 
-router.get('/:id', async (req, res) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id))
-    return res.status(404).send('Invalid ID');
-
+router.get('/:id', validateObjectId, async (req, res) => {
   const genre = await Genre.findById(req.params.id);
   if (!genre)
     return res
