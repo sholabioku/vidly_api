ec45ab7aea5543fee9b62d0aad6c7c2f3627766c
sholabioku <bilushonline@gmail.com>
2021-02-03 10:22:40 +0100

    Write integration test for GET /api/genres/:id  return the genre if valid id is passed

--- tests/integration/customers.test.js
+++ tests/integration/customers.test.js
@@ -16,18 +16,18 @@ describe('/api/customers', () => {
   describe('GET /', () => {
     it('should return all customers', async () => {
       await Customer.collection.insertMany([
-        { name: 'Lukman Bioku', isGold: true, phone: '08012345678' },
-        { name: 'Bilush', phone: '08012121212' },
+        { name: 'customer1', isGold: true, phone: '08012345678' },
+        { name: 'customer2', phone: '08012121212' },
       ]);
 
       const res = await request(server).get('/api/customers');
       expect(res.status).toBe(200);
       expect(res.body.length).toBe(2);
       expect(
-        res.body.some((customer) => customer.name === 'Lukman Bioku')
+        res.body.some((customer) => customer.name === 'customer1')
       ).toBeTruthy();
       expect(
-        res.body.some((customer) => customer.name === 'Bilush')
+        res.body.some((customer) => customer.name === 'customer2')
       ).toBeTruthy();
     });
   });
