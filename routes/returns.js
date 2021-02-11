1f13931e9ea54e49bfc5dde4c1ca260ecc6621f2
sholabioku <bilushonline@gmail.com>
2021-02-11 16:06:38 +0100

    Write POST /api/returns end point to check if movieId is provided functionality

--- routes/returns.js
+++ routes/returns.js
@@ -4,6 +4,9 @@ const router = express.Router();
 router.post('/', (req, res, next) => {
   if (!req.body.customerId)
     return res.status(400).send('CustomerId not provided');
+
+  if (!req.body.movieId) return res.status(400).send('MovieId not provided');
+
   res.status(401).send('Unauthorized!');
 });
 
