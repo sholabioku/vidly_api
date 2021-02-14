b1662d6de53c9ccc13e337f8e51337f5c9883435
sholabioku <bilushonline@gmail.com>
2021-02-14 17:31:56 +0100

    Add functionality to look up for rental in POST /api/returns route

--- routes/returns.js
+++ routes/returns.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', (req, res, next) => {
+const { Rental } = require('../models/rental');
+
+router.post('/', async (req, res, next) => {
   if (!req.body.customerId)
     return res.status(400).send('customerId not provided');
 
   if (!req.body.movieId) return res.status(400).send('movieId not provided');
+
+  const rental = await Rental.findOne({
+    'customer._id': req.body.customerId,
+    'movie._id': req.body.movieId,
+  });
+
+  console.log(rental);
+  if (!rental) return res.status(404).send('Rental not found!');
 
   res.status(401).send('Unauthorized!');
 });
