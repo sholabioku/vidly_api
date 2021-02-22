93a321f1317cacff4479717f8f4e78108ab70d20
sholabioku <bilushonline@gmail.com>
2021-02-22 15:21:47 +0100

    Write integration test for POST /api/rentals to return 401 if client is not logged in

--- tests/integration/rentals.test.js
+++ tests/integration/rentals.test.js
@@ -63,4 +63,55 @@ describe('/api/rentals', () => {
       expect(res.status).toBe(404);
     });
   });
+
+  describe('POST /', () => {
+    let token;
+    let customerId;
+    let movieId;
+    let rental;
+
+    const exec = async () => {
+      return await request(server)
+        .post('/api/rentals')
+        .set('x-auth-token', token)
+        .send({ customerId, movieId });
+    };
+
+    beforeEach(async () => {
+      customerId = mongoose.Types.ObjectId();
+      movieId = mongoose.Types.ObjectId();
+      token = new User().generateAuthToken();
+
+      // movie = new Movie({
+      //   _id: movieId,
+      //   title: '12345',
+      //   dailyRentalRate: 2,
+      //   genre: { name: '12345' },
+      //   numberInStock: 10,
+      // });
+
+      // await movie.save();
+
+      rental = new Rental({
+        customer: {
+          _id: customerId,
+          name: '12345',
+          phone: '12345',
+        },
+        movie: {
+          _id: movieId,
+          title: '12345',
+          dailyRentalRate: 2,
+        },
+      });
+      await rental.save();
+    });
+
+    it('should return 401 if client is not logged in', async () => {
+      token = '';
+
+      const res = await exec();
+      expect(res.status).toBe(401);
+    });
+  });
 });
